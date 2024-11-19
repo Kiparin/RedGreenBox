b128528ea58f81a2fa71a9ce431af7b0f4dd93e5
KiparinVAlex <446974890q@gmail.com>
2024-11-19 18:56:52 +0300

    Init

---
+++ RedGreenBox/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RedGreenBox
+//
+//  Created by Alexey Kiparin on 19.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
